c3b68a6f810f8cf53acf1299464997edd47470bc
sawan <sawanchauhanboss@gmail.com>
2022-03-01 14:50:58 +0400

    my tasks done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,10 @@
 class TasksController < ApplicationController
+    before_action :monster, except: [:index, :new, :create]
   def index
     @tasks = Task.all
   end
   def show
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
   end
   def new
     @task = Task.new
@@ -14,22 +15,23 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
   def edit
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
   end
   def update
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to task_path(@task)
   end
   def destroy
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path
   end
   private
   def task_params
-    # *Strong params*: You need to *whitelist* what can be updated by the user
-    # Never trust user data!
     params.require(:task).permit(:title, :details, :completed)
+  end
+  def monster
+    @task = Task.find(params[:id])
   end
 end
